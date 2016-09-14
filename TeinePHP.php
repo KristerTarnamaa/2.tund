5aa6fef51ef2f9ee5b1eff3be39953030fe72332
kristarn <kristarn@greeny.cs.tlu.ee>
2016-09-14 14:50:31 +0300

    added if and for loop

---
+++ TeinePHP.php
@@ -0,0 +1,26 @@
+<?php
+
+	$age = 5;
+	
+	if ($age <= 17) {
+		
+		echo "alaealine";
+		
+	} else {
+		
+		echo "täisealine";
+	}
+
+?>
+
+<br>
+
+<?php
+
+	for ( $i = 1; $i<=$age; $i++){
+		echo $i."palju ";
+	}
+	
+	echo "õnne";
+	
+?>
